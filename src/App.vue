da63a550eca46ba4e4a1fb6ac9dd520fe867ec4a
limichange <limichange@hotmail.com>
2017-07-20 12:49:57 +0800

    Add ele-ui

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <h3>原始资料</h3>
+    <el-input
+      type="textarea"
+      :rows="2"
+      placeholder="请输入内容"
+      v-model="textarea">
+    </el-input>
+
+    <h3>操作</h3>
+    <el-button>扫描</el-button>
+
+    <h3>原始码</h3>
+    <el-input placeholder="原始码1"></el-input>
+    <el-input placeholder="原始码2"></el-input>
+    <el-input placeholder="原始码3"></el-input>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
 export default {
   name: 'app',
-  components: {
-    Hello
+  data () {
+    return {
+      textarea: ''
+    }
+  },
+  methods: {
+    find () {}
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 960px;
+  margin: 0 auto;
 }
 </style>
